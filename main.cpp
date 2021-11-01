366f507ae2dabd0f85a5cc2a4e62612fc2b39202
hop- <hovhannespalyan@gmail.com>
2021-11-01 16:51:30 +0400

    Add cmd args support

--- main.cpp
+++ main.cpp
@@ -1,10 +1,37 @@
 #include "mazeCreator.hpp"
 
-int main()
+#include <string>
+#include <iostream>
+
+int main(int argc, char** argv)
 {
 	DFSMaze maze;
-	maze.resize(29, 29);
+
+    switch (argc) {
+        case 1:
+	        maze.resize(29, 29);
+            break;
+        case 3:
+            try {
+                int w = std::stoi(argv[1]);
+                int h = std::stoi(argv[2]);
+                maze.resize(w / 2 * 2 + 1, h / 2 * 2 + 1);
+            } catch(std::exception) {
+                std::cout << "Arguments should be integers" << std::endl;
+                return 1;
+            }
+            break;
+        default:
+            std::cout << "Wrong number of args" << std::endl;
+            std::cout << argv[0] << "[width height]" << std::endl;
+
+            return 1;
+    }
+
 	maze.create();
 	maze.dumpFile();
+
+    std::cout << "Done" << std::endl;
+
 	return 0;
 }
